8a2fa48de570e7e2b61fe8170a63afe4027b2865
xchillz <cendaaal@tutanota.com>
2024-07-03 20:19:17 -0400

    fix(player/xp): make right calculations

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3633,12 +3633,12 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
     }
 
     public static int calculateRequireExperience(int level) {
-        if (level < 16) {
-            return 2 * level + 7;
-        } else if (level >= 17 && level <= 31) {
-            return 5 * level - 38;
+        if (level >= 30) {
+            return 112 + (level - 30) * 9;
+        } else if (level >= 15) {
+            return 37 + (level - 15) * 5;
         } else {
-            return 9 * level - 158;
+            return 7 + level * 2;
         }
     }
 
